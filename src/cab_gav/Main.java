327b3414ccb6c3f9d562703a66754ae0946285d6
Andrej_Tesakov <gavguy3@gmail.com>
2018-10-23 19:32:32 +0300

    создали магию строки

--- src/cab_gav/Main.java
+++ src/cab_gav/Main.java
@@ -2,8 +2,14 @@ package cab_gav;
 
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -15,11 +21,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane root = new Pane();
+        // Pane root = new FlowPane()    //drug za druzkoj       ///sdelali okoshko
+        Pane root = new VBox();                                     //kazdij s novij stroki
+
+//        Button btn = new Button("Hello!");                  /// sozdali knopku
+        Label lbl = new Label("^>");
+        TextField txt = new TextField();                    //polje dlja voda dobavleno
+
+        lbl.textProperty().bind(Bindings.concat("HellO!, ", txt.textProperty(), "1"));
+
+//        btn.setOnAction(e -> {
+//            lbl.setText("Hello, " + txt.getText() + "1");
+//        }); //pri
+
+
+        root.getChildren().addAll( lbl, txt);                             //u rootu dobavili knopku
+
         Scene scene = new Scene(root, 500, 300);
         primaryStage.setTitle("Bulls and Covs");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();                                                ///zakoncili okosko
+
+
     }
 
 }
